82229d34b5695437016fccc7ada927eb8dc34e06
Mikalai Sokarau <nklskr@gmail.com>
2017-09-24 19:40:29 +0300

    added solution

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,27 +1,105 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+  constructor() {
+    this.length = 0;
+  }
 
-    append(data) {}
+  append(data) {
+    if (this.length === 0) {
+      const node = new Node(data);
+      this._head = node;
+      this._tail = node;
+    } else if (this.length === 1) {
+      this._tail = new Node(data, this._head);
+      this._head.next = this._tail;
+    } else {
+      const oldTail = this._tail;
+      const newTail = new Node(data, oldTail);
+      oldTail.next = newTail;
+      this._tail = newTail;
+    }
+    this.length++;
 
-    head() {}
+    return this;
+  }
 
-    tail() {}
+  head() {
+    return this._head.data;
+  }
 
-    at(index) {}
+  tail() {
+    return this._tail.data;
+  }
 
-    insertAt(index, data) {}
+  at(index) {
+    return findNode(this, index).data;
+  }
 
-    isEmpty() {}
+  insertAt(index, data) {
+    findNode(this, index).data = data;
 
-    clear() {}
+    return this;
+  }
 
-    deleteAt(index) {}
+  isEmpty() {
+    return this.length === 0;
+  }
 
-    reverse() {}
+  clear() {
+    this._head = new Node();
+    this._tail = new Node();
+    this.length = 0;
 
-    indexOf(data) {}
+    return this;
+  }
+
+  deleteAt(index) {
+    const elem = findNode(this, index),
+          prevElem = elem.prev,
+          nextElem = elem.next;
+
+    if (prevElem && nextElem) {
+      prevElem.next = nextElem;
+      nextElem.prev = prevElem;
+    }
+
+    return this;
+  }
+
+  reverse() {
+    let nodes = [],
+        current = this._head;
+
+    for (let i = 0; i < this.length; i++) {
+      nodes.push(current.data);
+      current = current.next;
+    }
+    nodes.reverse();
+    this.clear();
+    nodes.forEach(item => this.append(item));
+
+    return this;
+  }
+
+  indexOf(data) {
+    let current = this._head;
+    for (let i = 0; i < this.length; i++) {
+      if (current.data === data) return i;
+      current = current.next;
+    }
+
+    return -1;
+  }
+}
+
+function findNode(list, index) {
+  let current = list._head;
+  for (let i = 0; i < index; i++) {
+    current = current.next;
+  }
+
+  return current;
 }
 
 module.exports = LinkedList;
